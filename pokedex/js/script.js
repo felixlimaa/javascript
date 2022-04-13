29e563ce1d1fa11596d0c68096525f4e2c8e45f4
Felix Lima <contato.felixarruda@gmail.com>
2022-04-13 19:48:46 -0300

    Initial commit

---
+++ pokedex/js/script.js
@@ -0,0 +1,51 @@
+var quantidade = document.getElementById('quantidade');
+
+quantidade.addEventListener('keyup',()=>{
+    pegaPokemons(quantidade.value);
+})
+
+pegaPokemons(2);
+
+function pegaPokemons(quantidade){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit='+quantidade)
+    .then(response => response.json())
+    .then(allpokemon => {
+        
+        var pokemons = [];
+
+        allpokemon.results.map((val)=>{
+
+            fetch(val.url)
+                .then(response => response.json())
+                .then(pokemonSingle => {    
+
+                    pokemons.push({nome:val.name, imagem:pokemonSingle.sprites.front_default});
+
+                    if(pokemons.length == quantidade){
+
+                        var pokemonBoxes = document.querySelector('.pokemon-boxes');
+                        pokemonBoxes.innerHTML = "";
+
+                        pokemons.map((val)=>{
+
+                            pokemonBoxes.innerHTML += `
+                            
+                            <div class="pokemon-box">
+                                <img src="`+val.imagem+`"/>
+                                <p>`+val.nome+`</p>
+                            </div>
+                            `;
+
+                        })
+                        
+                        //console.log(pokemons);
+
+                    }
+            })  
+        })
+
+        pokemons.map((val)=>{
+            console.log(val.nome);
+        })
+    })
+}
